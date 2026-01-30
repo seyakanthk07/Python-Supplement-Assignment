a3f0a23242e18967aca6684e727e332ba10b0fed
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 16:05:41 +0530

    changes committed

--- problem_95.py
+++ problem_95.py
@@ -2,13 +2,24 @@
 # Find and fix the error
 
 def roman_to_int(s):
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_numerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
     total = 0
-    for i in range(len(s)):
-        if i < len(s) - 1 and roman[s[i]] < roman[s[i+1]]:
-            total -= roman[s[i]]
+    prev_value = 0
+    for char in reversed(s):
+        value = roman_numerals[char]
+        if value < prev_value:
+            total -= value
         else:
-            total += roman[s[i]]
+            total += value
+        prev_value = value
     return total
 
 print(f"XIV = {roman_to_int('XIV')}")
