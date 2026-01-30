5fc0bbad35fb263869d48e0e3eba7699be37d7c6
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 15:39:12 +0530

    committed

--- problem_65.py
+++ problem_65.py
@@ -2,12 +2,11 @@
 # Find and fix the error
 
 def has_duplicates(lst):
-    seen = []
+    seen = set()
     for item in lst:
         if item in seen:
             return True
-        seen.append(item)
+        seen.add(item)
     return False
-
 numbers = [1, 2, 3, 4, 5]
 print(f"Has duplicates: {has_duplicates(numbers)}")
