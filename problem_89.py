4efcbd1aff6407a11513dc68ead7ef96c1f027f5
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 15:59:55 +0530

    changes committed

--- problem_89.py
+++ problem_89.py
@@ -2,12 +2,8 @@
 # Find and fix the error
 
 def is_palindrome_number(n):
-    original = n
-    reversed_num = 0
-    while n > 0:
-        digit = n % 10
-        reversed_num = reversed_num * 10 + digit
-        n = n // 10
+    original = str(n)
+    reversed_num = original[::-1]
     return original == reversed_num
 
 print(f"Is 121 palindrome? {is_palindrome_number(121)}")
