fde1c41f12fd30738e41e819bf16f034e6ab7d19
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 16:00:47 +0530

    changes committed

--- problem_91.py
+++ problem_91.py
@@ -4,11 +4,11 @@
 def selection_sort(arr):
     n = len(arr)
     for i in range(n):
-        min_idx = i
+        min_index = i
         for j in range(i+1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
 numbers = [64, 25, 12, 22, 11]
