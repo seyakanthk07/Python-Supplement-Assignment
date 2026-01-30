a96be85dacc872037ac4830f54681ed8f7cb137b
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 15:59:02 +0530

    changes committed

--- problem_87.py
+++ problem_87.py
@@ -6,7 +6,7 @@ def pascals_triangle(n):
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
     return triangle
 
