68c770dee0775b5eb7bb56eb1f489f038c7814ee
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 15:48:06 +0530

    changes committed

--- problem_73.py
+++ problem_73.py
@@ -4,14 +4,13 @@
 def max_difference(arr):
     if len(arr) < 2:
         return 0
-    min_val = arr[0]
-    max_diff = 0
+    min_elem = arr[0]
+    max_diff = arr[1] - arr[0]
     for i in range(1, len(arr)):
-        if arr[i] - min_val > max_diff:
-            max_diff = arr[i] - min_val
-        if arr[i] < min_val:
-            min_val = arr[i]
+        if arr[i] - min_elem > max_diff:
+            max_diff = arr[i] - min_elem
+        if arr[i] < min_elem:
+            min_elem = arr[i]
     return max_diff
-
 numbers = [7, 1, 5, 3, 6, 4]
 print(f"Max difference: {max_difference(numbers)}")
