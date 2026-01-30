7a58b0fbff9f280e0c6509ce7b833bd0b035cc27
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 15:34:08 +0530

    changes committed

--- problem_60.py
+++ problem_60.py
@@ -4,9 +4,6 @@
 def is_armstrong(n):
     num_str = str(n)
     num_digits = len(num_str)
-    total = 0
-    for digit in num_str:
-        total += int(digit) ** num_digits
+    total = sum(int(digit) ** num_digits for digit in num_str)
     return total == n
-
 print(f"Is 153 Armstrong? {is_armstrong(153)}")
