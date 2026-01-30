2276e69fbb4cce9df9500060e9503df4f160f055
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 15:51:51 +0530

    changes committed

--- problem_77.py
+++ problem_77.py
@@ -2,8 +2,10 @@
 # Find and fix the error
 
 def is_perfect_square(n):
-    sqrt = n ** 0.5
-    return sqrt == int(sqrt)
+    if n < 0:
+        return False
+    root = int(n**0.5)
+    return root * root == n
 
 print(f"Is 16 perfect square? {is_perfect_square(16)}")
 print(f"Is 15 perfect square? {is_perfect_square(15)}")
