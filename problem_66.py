d6913b8883e9af9d23fc9ca3225f95e19973e39d
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 15:39:58 +0530

    committed

--- problem_66.py
+++ problem_66.py
@@ -2,11 +2,7 @@
 # Find and fix the error
 
 def intersection(list1, list2):
-    result = []
-    for item in list1:
-        if item in list2:
-            result.append(item)
-    return result
+    return list(set(list1) & set(list2))
 
 l1 = [1, 2, 3, 4, 5]
 l2 = [3, 4, 5, 6, 7]
