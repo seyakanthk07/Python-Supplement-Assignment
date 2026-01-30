cddb662a5c5081a55d336f66c5203686530ef9db
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 15:58:26 +0530

    changes committed

--- problem_86.py
+++ problem_86.py
@@ -2,11 +2,13 @@
 # Find and fix the error
 
 def diagonal_sum(matrix):
-    n = len(matrix)
     total = 0
+    n = len(matrix)
     for i in range(n):
-        total += matrix[i][i]
-        total += matrix[i][n-1-i]
+        total += matrix[i][i]  # Primary diagonal
+        total += matrix[i][n - 1 - i]  # Secondary diagonal
+    if n % 2 == 1:
+        total -= matrix[n // 2][n // 2]  # Subtract the middle element if n is odd
     return total
 
 mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
