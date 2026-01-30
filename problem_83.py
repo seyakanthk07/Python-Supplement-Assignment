92d90396661505b1145f1ce15b7826e7aeb20a1b
seyakanthk07 <seyakanthk@gmail.com>
2026-01-30 15:56:56 +0530

    changes committed

--- problem_83.py
+++ problem_83.py
@@ -2,8 +2,10 @@
 # Find and fix the error
 
 def kth_smallest(arr, k):
+    if k <= 0 or k > len(arr):
+        return None
     sorted_arr = sorted(arr)
-    return sorted_arr[k]
+    return sorted_arr[k-1]
 
 numbers = [7, 10, 4, 3, 20, 15]
 print(f"3rd smallest: {kth_smallest(numbers, 3)}")
